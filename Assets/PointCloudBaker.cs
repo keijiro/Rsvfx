786e97097d0a9adb87030494e9282f0824656638
Keijiro Takahashi <keijiro@gmail.com>
2019-02-19 19:13:32 +0900

    Refactoring and color map support

--- Assets/PointCloudBaker.cs
+++ Assets/PointCloudBaker.cs
@@ -7,157 +7,162 @@ namespace Rsvfx
 {
     public class PointCloudBaker : MonoBehaviour
     {
-        [SerializeField] RsFrameProvider _source = null;
-        [SerializeField] ComputeShader _compute = null;
-        [SerializeField] RenderTexture _output = null;
+        #region Editable attributes
 
-        FrameQueue _queue;
-        ComputeBuffer _buffer;
-        RenderTexture _texture;
+        [SerializeField] RsFrameProvider _colorSource = null;
+        [SerializeField] RsFrameProvider _pointSource = null;
+        [Space]
+        [SerializeField] RenderTexture _colorMap = null;
+        [SerializeField] RenderTexture _positionMap = null;
+
+        [SerializeField, HideInInspector] ComputeShader _compute = null;
+
+        #endregion
+
+        #region Private fields
+
+        FrameQueue _colorQueue;
+        FrameQueue _pointQueue;
+
+        ComputeBuffer _colorBuffer;
+        ComputeBuffer _positionBuffer;
+        ComputeBuffer _remapBuffer;
+
+        Vector2Int _dimensions;
+        RenderTexture _tempColorMap;
+        RenderTexture _tempPositionMap;
+
+        #endregion
 
         #region MonoBehaviour implementation
 
         void Start()
         {
-            _queue = new FrameQueue(1);
-            _source.OnNewSample += OnNewSample;
+            _colorQueue = new FrameQueue(1);
+            _pointQueue = new FrameQueue(1);
+
+            _colorSource.OnNewSample += OnNewColorSample;
+            _pointSource.OnNewSample += OnNewPointSample;
         }
 
         void OnDestroy()
         {
-            if (_queue != null)
+            if (_colorQueue != null)
             {
-                _queue.Dispose();
-                _queue = null;
+                _colorQueue.Dispose();
+                _colorQueue = null;
             }
 
-            if (_buffer != null)
+            if (_pointQueue != null)
             {
-                _buffer.Dispose();
-                _buffer = null;
+                _pointQueue.Dispose();
+                _pointQueue = null;
             }
 
-            if (_texture != null)
+            if (_colorBuffer != null)
             {
-                Destroy(_texture);
-                _texture = null;
+                _colorBuffer.Dispose();
+                _colorBuffer = null;
+            }
+
+            if (_positionBuffer != null)
+            {
+                _positionBuffer.Dispose();
+                _positionBuffer = null;
+            }
+
+            if (_remapBuffer != null)
+            {
+                _remapBuffer.Dispose();
+                _remapBuffer = null;
+            }
+
+            if (_tempColorMap != null)
+            {
+                Destroy(_tempColorMap);
+                _tempColorMap = null;
+            }
+
+            if (_tempPositionMap != null)
+            {
+                Destroy(_tempPositionMap);
+                _tempPositionMap = null;
             }
         }
 
         void Update()
         {
-            using (var points = DequeuePoints())
-            {
-                if (points == null) return;
-                if (points.VertexData == System.IntPtr.Zero) return;
-                if (_output == null) return;
+            using (var frame = DequeueColorFrame())
+                if (frame != null) UpdateColorData(frame);
 
-                if (_buffer == null || _buffer.count != points.Count * 3)
-                    ResetBuffers(points);
+            using (var frame = DequeuePointFrame())
+                if (frame != null) UpdatePointData(frame);
 
-                if (!CheckConsistency())
-                {
-                    enabled = false;
-                    return;
-                }
+            if (_colorBuffer == null) return;
+            if (_positionBuffer == null) return;
+            if (_remapBuffer == null) return;
 
-                ComputeBufferUtility.SetUnmanagedData
-                    (_buffer, points.VertexData, points.Count * 3, sizeof(float));
+            if (_colorMap == null) return;
+            if (_positionMap == null) return;
 
-                _compute.SetBuffer(0, "Input", _buffer);
-                _compute.SetTexture(0, "Output", _texture);
-                _compute.Dispatch(0, 1, _texture.height, 1);
+            if (!CheckConsistency()) return;
 
-                Graphics.CopyTexture(_texture, _output);
-            }
-        }
+            RemapPoints();
 
-        #endregion
-
-        #region Private properties and methods
-
-        const int _textureWidth = 256;
-
-        bool CheckConsistency()
-        {
-            uint x, y, z;
-            _compute.GetKernelThreadGroupSizes(0, out x, out y, out z);
-            if (x != _textureWidth || y != 1 || z != 1)
-            {
-                Debug.LogError(
-                    "Compute kernel thread group sizes are wrong. " +
-                    "They should be (" + _textureWidth + ", 1, 1)."
-                );
-                return false;
-            }
-
-            if (_output.width != _texture.width || _output.height != _texture.height)
-            {
-                Debug.LogError(
-                    "Texture dimensions are wrong. " +
-                    "They should be (" + _texture.width + "x" + _texture.height + ")."
-                );
-                return false;
-            }
-
-            if (_output.format != RenderTextureFormat.ARGBHalf)
-            {
-                Debug.LogError(
-                    "Render texture format is wrong. It should be ARGBHalf."
-                );
-                return false;
-            }
-
-            return true;
-        }
-
-        Points DequeuePoints()
-        {
-            Frame frame;
-            return _queue.PollForFrame(out frame) ? (Points)frame : null;
-        }
-
-        void ResetBuffers(Points points)
-        {
-            if (_buffer != null) _buffer.Dispose();
-            if (_texture != null) Destroy(_texture);
-
-            _buffer = new ComputeBuffer(points.Count * 3, sizeof(float));
-
-            _texture = new RenderTexture(
-                _textureWidth, points.Count / _textureWidth, 0,
-                RenderTextureFormat.ARGBHalf
-            );
-            _texture.enableRandomWrite = true;
-            _texture.Create();
+            Graphics.CopyTexture(_tempColorMap, _colorMap);
+            Graphics.CopyTexture(_tempPositionMap, _positionMap);
         }
 
         #endregion
 
         #region Frame provider callback
 
-        void OnNewSample(Frame frame)
+        void OnNewColorSample(Frame frame)
         {
-            try
-            {
-                using (var points = RetrievePoints(frame))
-                {
-                    if (points != null) _queue.Enqueue(points);
-                }
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogException(e);
-            }
+            using (var cf = RetrieveColorFrame(frame))
+                if (cf != null) _colorQueue.Enqueue(cf);
+        }
 
-            frame.Dispose();
+        void OnNewPointSample(Frame frame)
+        {
+            using (var pf = RetrievePointFrame(frame))
+                if (pf != null) _pointQueue.Enqueue(pf);
         }
 
         #endregion
 
         #region Frame query method
 
-        Points RetrievePoints(Frame frame)
+        VideoFrame RetrieveColorFrame(Frame frame)
+        {
+            if (frame is VideoFrame)
+            {
+                using (var profile = frame.Profile)
+                {
+                    if (profile.Stream == Stream.Color &&
+                        profile.Format == Format.Rgba8 &&
+                        profile.Index == 0)
+                        return (VideoFrame)frame;
+                }
+            }
+
+            if (frame.IsComposite)
+            {
+                using (var fset = FrameSet.FromFrame(frame))
+                {
+                    foreach (var f in fset)
+                    {
+                        var ret = RetrieveColorFrame(f);
+                        if (ret != null) return ret;
+                        f.Dispose();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        Points RetrievePointFrame(Frame frame)
         {
             if (frame is Points) return (Points)frame;
 
@@ -167,13 +172,165 @@ namespace Rsvfx
                 {
                     foreach (var f in fset)
                     {
-                        if (f is Points) return (Points)f;
+                        var ret = RetrievePointFrame(f);
+                        if (ret != null) return ret;
                         f.Dispose();
                     }
                 }
             }
 
             return null;
+        }
+
+        #endregion
+
+        #region Private properties and methods
+
+        VideoFrame DequeueColorFrame()
+        {
+            Frame frame;
+            return _colorQueue.PollForFrame(out frame) ? (VideoFrame)frame : null;
+        }
+
+        Points DequeuePointFrame()
+        {
+            Frame frame;
+            return _pointQueue.PollForFrame(out frame) ? (Points)frame : null;
+        }
+
+        void UpdateColorData(VideoFrame frame)
+        {
+            if (frame.Data == System.IntPtr.Zero) return;
+
+            var size = frame.Width * frame.Height;
+
+            if (_colorBuffer != null && _colorBuffer.count != size)
+            {
+                _colorBuffer.Dispose();
+                _colorBuffer = null;
+            }
+
+            if (_colorBuffer == null)
+                _colorBuffer = new ComputeBuffer(size, 4);
+
+            UnsafeUtility.SetUnmanagedData (_colorBuffer, frame.Data, size, 4);
+
+            _dimensions = new Vector2Int(frame.Width, frame.Height);
+        }
+
+        void UpdatePointData(Points points)
+        {
+            if (points.VertexData == System.IntPtr.Zero) return;
+            if (points.TextureData == System.IntPtr.Zero) return;
+
+            var countx2 = points.Count * 2;
+            var countx3 = points.Count * 3;
+
+            if (_positionBuffer != null && _positionBuffer.count != countx3)
+            {
+                _positionBuffer.Dispose();
+                _positionBuffer = null;
+            }
+
+            if (_remapBuffer != null && _remapBuffer.count != countx2)
+            {
+                _remapBuffer.Dispose();
+                _remapBuffer = null;
+            }
+
+            if (_positionBuffer == null)
+                _positionBuffer = new ComputeBuffer(countx3, sizeof(float));
+
+            if (_remapBuffer == null)
+                _remapBuffer = new ComputeBuffer(countx2, sizeof(float));
+
+            UnsafeUtility.SetUnmanagedData
+                (_positionBuffer, points.VertexData, countx3, sizeof(float));
+
+            UnsafeUtility.SetUnmanagedData
+                (_remapBuffer, points.TextureData, countx2, sizeof(float));
+        }
+
+        void RemapPoints()
+        {
+            if (_tempColorMap != null &&
+                (_tempColorMap.width != _dimensions.x ||
+                 _tempColorMap.height != _dimensions.y))
+            {
+                Destroy(_tempColorMap);
+                _tempColorMap = null;
+            }
+
+            if (_tempPositionMap != null &&
+                (_tempPositionMap.width != _dimensions.x ||
+                 _tempPositionMap.height != _dimensions.y))
+            {
+                Destroy(_tempPositionMap);
+                _tempPositionMap = null;
+            }
+
+            if (_tempColorMap == null)
+            {
+                _tempColorMap = new RenderTexture
+                    (_dimensions.x, _dimensions.y, 0, RenderTextureFormat.ARGB32);
+                _tempColorMap.enableRandomWrite = true;
+                _tempColorMap.Create();
+            }
+
+            if (_tempPositionMap == null)
+            {
+                _tempPositionMap = new RenderTexture
+                    (_dimensions.x, _dimensions.y, 0, RenderTextureFormat.ARGBHalf);
+                _tempPositionMap.enableRandomWrite = true;
+                _tempPositionMap.Create();
+            }
+
+            _compute.SetInts("MapDimensions", _dimensions);
+            _compute.SetBuffer(0, "ColorBuffer", _colorBuffer);
+            _compute.SetBuffer(0, "PositionBuffer", _positionBuffer);
+            _compute.SetBuffer(0, "RemapBuffer", _remapBuffer);
+            _compute.SetTexture(0, "ColorMap", _tempColorMap);
+            _compute.SetTexture(0, "PositionMap", _tempPositionMap);
+            _compute.Dispatch(0, _dimensions.x / 8, _dimensions.y / 8, 1);
+        }
+
+        bool _warned;
+
+        bool CheckConsistency()
+        {
+            if (_warned) return false;
+
+            if (_dimensions.x % 8 != 0 && _dimensions.y % 8 != 0)
+            {
+                Debug.LogError("Color input dimensions should be a multiple of 8.");
+                _warned = true;
+            }
+
+            if (_colorMap.width != _dimensions.x || _colorMap.height != _dimensions.y)
+            {
+                Debug.LogError("Color map dimensions don't match with the input.");
+                _warned = true;
+            }
+
+            if (_positionMap.width != _dimensions.x || _positionMap.height != _dimensions.y)
+            {
+                Debug.LogError("Position map dimensions don't match with the input.");
+                _warned = true;
+            }
+
+            if (_colorMap.format != RenderTextureFormat.ARGB32)
+            {
+                Debug.LogError("Color map format should be ARGB32");
+                _warned = true;
+            }
+
+            if (_positionMap.format != RenderTextureFormat.ARGBHalf)
+            {
+                Debug.LogError("Position map format should be ARGBHalf");
+                _warned = true;
+            }
+
+            return !_warned;
         }
 
         #endregion
